d8a66be0e879c38de13ec21471563c19df090cd8
mxbooll <mxbooll@gmail.com>
2020-05-05 13:30:38 +0500

    basic alice request

---
+++ AliceHook/Controller/AliceController.cs
@@ -0,0 +1,46 @@
+﻿
+using AliceHook.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AliceHook.Controller
+{
+    [ApiController]
+    [Route("/")]
+    public class AliceController : ControllerBase
+    {
+        private static readonly JsonSerializerSettings ConverterSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new SnakeCaseNamingStrategy()
+            }
+        };
+
+        [HttpGet]
+        public string Get()
+        {
+            return "It works!";
+        }
+
+        [HttpPost]
+        public Task Post()
+        {
+            using var reader = new StreamReader(Request.Body);
+            var body = reader.ReadToEnd();
+
+            var aliceRequest = JsonConvert.DeserializeObject<AliceRequest>(body, ConverterSettings);
+            var aliceResponce = new AliceResponce(aliceRequest)
+            {
+                Response = { Text = "Привет"}
+            };
+
+            var stringResponce = JsonConvert.SerializeObject(aliceResponce, ConverterSettings);
+            return Response.WriteAsync(stringResponce);
+        }
+    }
+}
---
+++ AliceHook/Models/AliceResponce.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace AliceHook.Models
+{
+    public class Button
+    {
+        public string Title { get; set; }
+        public Dictionary<string, string> Payload { get; set; }
+        public string Url { get; set; }
+        public bool Hide { get; set; }
+    }
+
+    public class Response
+    {
+        public string Text { get; set; }
+        public string Tts { get; set; }
+        public List<Button> Buttons { get; set; }
+        public bool EndSession { get; set; }
+    }
+
+    public class AliceResponce
+    {
+        public Response Response { get; set; } = new Response();
+        public Session Session { get; set; }
+        public string Version { get; set; }
+
+        public AliceResponce(AliceRequest request)
+        {
+            Session = request.Session;
+        }
+    }
+}
